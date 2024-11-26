bb0f380898b07cb2a5c70f3bd237b444c1878683
Abdullahi Hussein Abdikariim <cabdulahihussein067@gmail.com>
2024-11-26 18:05:19 +0000

    Update Main.js

--- Calculation/Main.js
+++ Calculation/Main.js
@@ -11,18 +11,3 @@
 //             input.value = string;
 //         }
 
-//         else if(e.target.innerHTML == 'AC'){
-//             string = "";
-//             input.value = string;
-//         }
-//         else if(e.target.innerHTML == 'DEL'){
-//             string = string.substring(0, string.length-1);
-//             input.value = string;
-//         }
-//         else{
-//             string += e.target.innerHTML;
-//             input.value = string;
-//         }
-        
-//     })
-// })
